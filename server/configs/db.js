7372863d9fdd822f07fc1637003be3cbf3a040d0
amanverma9008003 <amanverma0039008@gmail.com>
2025-11-18 09:13:14 +0530

    updates

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(`${process.env.MONGO_URI}/quickshow`);
+        const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (err) {
     console.error(err.message);
